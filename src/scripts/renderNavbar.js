06bb342bbb50211d4cd4ecd056cf7474404611f0
MyMac <nagornyykostya@gmail.com>
2020-02-10 19:40:34 +0200

    nav-bar current date selected added

--- src/scripts/renderNavbar.js
+++ src/scripts/renderNavbar.js
@@ -1,15 +1,21 @@
 import { initCalendar } from './storage.js'
 
 export const renderNavbar = () => {
-    const navBar = document.querySelector(".navigation__grid");
-    let arrNavBarElements = [];
-    for (let i = 0; i < initCalendar.weekDays.length; i++) {
-        let datesPerWeek = initCalendar.selectedWeek + (i * initCalendar.miliSecondsPerDay);
-        arrNavBarElements.push(`
+  const navBar = document.querySelector(".navigation__grid");
+  let arrNavBarElements = [];
+  for (let i = 0; i < initCalendar.weekDays.length; i++) {
+    let datesPerWeek = initCalendar.selectedWeek + (i * initCalendar.miliSecondsPerDay);
+    arrNavBarElements.push(`
         <div class="navigation__day">
           <span class="navigation__day-of-week">${initCalendar.weekDays[i]}</span>
-          <span class="navigation__date-of-month">${new Date(datesPerWeek).getDate()}</span>
+          <span class="navigation__date-of-month 
+          ${new Date().getDate() ===
+        new Date(datesPerWeek).getDate() &&
+        new Date().getMonth() === new Date(datesPerWeek).getMonth() ?
+        `navigation__date-of-month_selected` :
+        ``}">${new Date(datesPerWeek).getDate()}</span>
         </div>`);
-    }
-    navBar.innerHTML = arrNavBarElements.join('')
-}
+  }
+
+  navBar.innerHTML = arrNavBarElements.join('')
+  }
